5bafc00095ed38be785dee71d0f7eb68ce77fbb6
Kaplc <755536175@qq.com>
2023-12-02 00:42:12 +0800

    添加Dotween插件, 替换开始面板动画移动

---
+++ Assets/Scripts/Application/MVC/View/BeginScene/Object/Other/Bird.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Bird : MonoBehaviour
+{
+    public float time;
+    private Tween tween;
+    
+    private void Start()
+    {
+        //创建移动动画
+        tween = transform.DOLocalMoveY(((RectTransform)transform).anchoredPosition.y + 50f, time);
+        // 设置循环移动
+        tween.SetLoops(-1, LoopType.Yoyo); 
+    }
+
+    private void OnDestroy()
+    {
+        tween?.Kill();
+    }
+}
---
+++ Assets/Scripts/Application/MVC/View/BeginScene/Object/Other/Cloud.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float time;
+    private Tween tween;
+    
+    private void Start()
+    {
+        tween = transform.DOLocalMoveX(((RectTransform)transform).anchoredPosition.x + 1920f, time);
+        tween.SetLoops(-1, LoopType.Restart);
+        tween.SetEase(Ease.Linear);
+    }
+
+    private void OnDestroy()
+    {
+        tween?.Kill();
+    }
+}
